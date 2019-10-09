34506b42555c956a19d55b89b32cedc252d42ae0
Autri Baghkhanian <autri.baghkhanian@gmail.com>
2019-10-08 21:15:04 -0700

    all the basics working

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
-const card = document.querySelector('.card');
+// ------------------------------------------
+//  GLOBAL VARIABLES
+// ------------------------------------------
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`;
+const flexContainer = document.querySelector(".flex-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-function fetchData(url) {
-    return fetch(url)
+fetch(urlAPI)
       .then(checkStatus)
       .then(res => res.json())
+      .then(res => res.results)
+      .then(displayEmployees)
       .catch(err => console.log('Something has gone super wrong.', err));
-  }
-  
-  Promise.all([
-    fetchData('https://randomuser.me/api/?results=12')
-  ])
-  .then(data => {
-    // console.log(data);
-    getDetails(data);
-    // console.log(data.results);
-    // console.log(data.results[5].name.first);
-  })
 
 
 // ------------------------------------------
@@ -31,15 +30,53 @@ function checkStatus(response) {
     } else {
       return Promise.reject(new Error(response.statusText));
     }
-};
+}
 
-function getDetails(data) {
-    const details = data.map( person => {
-        console.log(person.results[2].name.first);
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    let employeeHTML = '';
+    employees.forEach((employee, index) => {
+        let name = employee.name.first + ' ' + employee.name.last;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture.large;
+        employeeHTML += `
+            <div class="card" data-index="${index}">
+                <img class="photo" src="${picture}" alt="${name}">
+                <p class="name">${name}</p>
+                <p class="email">${email}</p>
+                <p class="city">${city}</p>
+            </div>
+        `
     });
-};
 
+    flexContainer.innerHTML = employeeHTML;
+}
 
+function displayModal(index) {
+
+    let { name, dob, phone, email, location: { city, street, state, postcode
+    }, picture } = employees[index];
+
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+        <img class="photo" src="${picture.large}" />
+            <div class="text-container">
+                <h2 class="name">${name.first} ${name.last}</h2>
+                <p class="email">${email}</p>
+                <p class="address">${city}</p>
+                <hr />
+                <p>${phone}</p>
+                <p class="address">${street}, ${state} ${postcode}</p>
+                <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+            </div>
+    `;
+
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+
+}
 
 
 
@@ -47,6 +84,15 @@ function getDetails(data) {
 //  EVENT LISTENERS
 // ------------------------------------------
 
-card.addEventListener('click', (e) => {
-    console.log('Clicked: ' + e.target);
+flexContainer.addEventListener('click', (e) => {
+    if (e.target !== flexContainer) {
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
 });
